ddb7f3f3e71799821d69abd142d06086889d7ec9
Duy Le <dnbl@Duys-MacBook-Pro.local>
2021-08-29 00:15:25 +0700

    Dictionary app built with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Switch, withStyles } from '@material-ui/core';
 import './App.css';
+import { Container } from '@material-ui/core';
+import Header from './components/Header/Header';
+import Definitions from './components/Definitions/Definitions';
+import { grey } from '@material-ui/core/colors';
 
 function App() {
+
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const [category, setCategory] = useState("en");
+  const [LightMode, setLightMode] = useState(false);
+
+  const DarkMode = withStyles({
+    switchBase: {
+      color: grey[300],
+      '&$checked': {
+        color: grey[500],
+      },
+      '&$checked + $track': {
+        backgroundColor: grey[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+
+
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`);
+      setMeanings(data.data);
+      console.log(meanings);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    dictionaryApi();
+  }, [word, category])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+      className="App"
+      style={{ 
+        height: "100vh",
+        backgroundColor: LightMode ? "#fff": "#282c34",
+        color: LightMode? "black": "white",
+        transition: "all 0.5s linear"
+      }}
+    >
+      <Container maxWidth="md" style={{ display: "flex", flexDirection: "column", height: "100vh", justifyContent: "space-evenly" }}>
+        <div style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}> 
+        <span>{LightMode ? 'Dark' : 'Light'} Mode</span>
+          <DarkMode checked={LightMode} onChange={() => setLightMode(!LightMode)} />
+        </div>
+        <Header
+          category={category}
+          setCategory={setCategory}
+          word={word}
+          setWord={setWord}
+          LightMode={LightMode}
+        />
+        { meanings && <Definitions
+          word={word}
+          category={category}
+          meanings={meanings}
+        />}
+      </Container>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { createTheme, ThemeProvider, TextField, MenuItem } from '@material-ui/core';
+import categories from '../../data/category';
+import './Header.css';
+
+const Header = ( {category, setCategory, word, setWord, LightMode }) => {
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: LightMode? "#000": '#fff',
+            },
+            type: LightMode? 'light': 'dark',
+        },
+    });
+
+    const handleChange = (language) => {
+        setCategory(language);
+        setWord('');
+    }
+
+    return (
+        <div className="header">
+            <span className="title">Word Hunt </span>
+            <div className="inputs">
+                <ThemeProvider theme={darkTheme}>
+                    <TextField 
+                        className="search"
+                        label="Search a word"
+                        value={word}
+                        onChange={(e) => setWord(e.target.value)}
+                    />
+                    <TextField
+                        className="select"
+                        select
+                        label="Language"
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                        >
+                            {categories.map(option => (
+                                <MenuItem key={option.label} value={option.label}>
+                                    {option.value}
+                                </MenuItem>
+                            ))}
+                    </TextField>
+                </ThemeProvider>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
